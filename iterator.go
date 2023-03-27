2b214be84d2c23905d2a163307e4abceda9099bf
boris <boris.lok@cienet.com>
2023-03-27 10:31:15 +0800

    [Doc] add `doc` and `usage`

--- iterator.go
+++ iterator.go
@@ -55,12 +55,25 @@ func (f *FilterIterator[T]) Item() T {
 	return f.s.Item()
 }
 
+// IntoIterator Turn an array into an iterator
+// Usage:
+// ```
+// var numbers []int = []int{1,2,3}
+// iter := IntoIterator(numbers)
+// ```
 func IntoIterator[T any](elements []T) Iterator[T] {
 	return &SliceIterator[T]{
 		Elements: elements,
 	}
 }
 
+// Map transforms the iterator into another, and then apply the mapper function to each element
+// Usage:
+// ```
+// var numbers []int = []int{1,2,3}
+// iter := IntoIterator(numbers)
+// mapped := Collect(Map(func (x int) int { return x * x }, iter))
+// ```
 func Map[T any, U any](mapper func(T) U, iter Iterator[T]) Iterator[U] {
 	return &MappingIterator[T, U]{
 		s:      iter,
@@ -68,6 +81,13 @@ func Map[T any, U any](mapper func(T) U, iter Iterator[T]) Iterator[U] {
 	}
 }
 
+// Filter use predicate function to check each element in the iterator
+// Usage:
+// ```
+// var numbers []int = []int {1,2,3}
+// iter := IntoIterator(numbers)
+// filtered := Collect(Filter(func (x int) bool { return x % 2 == 0 }, iter)
+// ```
 func Filter[T any](pred func(T) bool, iter Iterator[T]) Iterator[T] {
 	return &FilterIterator[T]{
 		s:    iter,
@@ -75,6 +95,13 @@ func Filter[T any](pred func(T) bool, iter Iterator[T]) Iterator[T] {
 	}
 }
 
+// Collect Turn an iterator into a array
+// Usage:
+// ```
+// var numbers []int = []int {1,2,3}
+// iter := IntoIterator(numbers)
+// arr := Collect(iter)
+// ```
 func Collect[T any](iter Iterator[T]) []T {
 	var res []T
 
@@ -85,6 +112,13 @@ func Collect[T any](iter Iterator[T]) []T {
 	return res
 }
 
+// Reduce the elements to a single one.
+// Usage:
+// ```
+// var numbers []int = []int {1,2,3}
+// iter := IntoIterator(numbers)
+// ans := Reduce(0, func(prev int, cur int) int { return prev + cur }, iter)
+// ```
 func Reduce[T any, U any](initial U, reducer func(U, T) U, iter Iterator[T]) U {
 	var initialValue = initial
 
